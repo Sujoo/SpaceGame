0d3cb8217f1ca21cec30e388e952baad65c4e49b
Sujoo <sujoowow@gmail.com>
2014-05-04 13:39:30 -0400

    added wallet

---
+++ SpaceGame/src/sujoo/games/spacegame/datatypes/Wallet.java
@@ -0,0 +1,26 @@
+package sujoo.games.spacegame.datatypes;
+
+public class Wallet {
+	private int credits;
+	
+	public Wallet(int credits) {
+		this.credits = credits;
+	}
+	
+	public int getCredits() {
+		return credits;
+	}
+	
+	public boolean removeCredits(int amount) {
+		if (credits >= amount) {
+			credits -= amount;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void addCredits(int amount) {
+		credits += amount;
+	}
+}
